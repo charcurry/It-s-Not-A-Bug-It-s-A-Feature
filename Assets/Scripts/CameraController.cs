5a7415569b94665e6ff5d5a3fa42bac7795adb62
Dylan Adams <dylan.adams.2016@outlook.com>
2024-04-25 10:45:23 -0300

    Added the Beta Player Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [Header("Properties")]
+    public float mouseSensitivity = 500f;
+
+    [Header("References")]
+    public Transform playerBody;
+
+    [HideInInspector] public float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, transform.rotation.eulerAngles.z);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
