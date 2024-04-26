be69093edb115d1a9602c8b13f91b0096eac3ed9
Harjot Singh <121034602+HarjotSinghGillHL@users.noreply.github.com>
2024-04-26 13:38:40 -0300

    Update UX  Callbacks

--- Assets/Scripts/UX/UX_Callbacks.cs
+++ Assets/Scripts/UX/UX_Callbacks.cs
@@ -5,6 +5,76 @@ using UnityEngine;
 public class UX_ButtonCallbacks : MonoBehaviour
 {
     // Start is called before the first frame update
+  
+    public GameObject CanvasBackground;
+    public GameObject MainMenu;
+    public GameObject OptionsScreen;
+    public GameObject IngameOverlay;
+    public GameObject PausedOverlay;
+
+
+
+    public enum EUICurrentState
+    {
+        UI_STATE_MENU = 0,
+        UI_STATE_PAUSED,
+        UI_STATE_OPTIONS,
+        UI_STATE_INGAME,
+        UI_STATE_MAX,
+    }
+    public enum EUXCallbacksNoParameter
+    {
+        BUTTON_CALLBACK_PLAY =0 ,
+        BUTTON_CALLBACK_EXIT,
+        BUTTON_CALLBACK_OPTIONS,
+        BUTTON_CALLBACK_RETURN_TO_MENU,
+    }
+
+    private EUICurrentState UIState = EUICurrentState.UI_STATE_MENU;
+    private EUICurrentState PrevUIState = EUICurrentState.UI_STATE_MENU;
+    void OnUIStateChange(EUICurrentState _UIState)
+    {
+        PrevUIState = UIState;
+        UIState = _UIState;
+
+        CanvasBackground.SetActive(UIState != EUICurrentState.UI_STATE_INGAME);
+        MainMenu.SetActive(UIState == EUICurrentState.UI_STATE_MENU);
+        OptionsScreen.SetActive(UIState == EUICurrentState.UI_STATE_OPTIONS);
+        IngameOverlay.SetActive(UIState == EUICurrentState.UI_STATE_INGAME);
+        PausedOverlay.SetActive(UIState == EUICurrentState.UI_STATE_PAUSED);
+    }
+
+    public void OnUXCallback(EUXCallbacksNoParameter eCallback)
+    {
+        switch (eCallback)
+        {
+            case EUXCallbacksNoParameter.BUTTON_CALLBACK_PLAY:
+                {
+
+                    break;
+                }
+            case EUXCallbacksNoParameter.BUTTON_CALLBACK_EXIT:
+                {
+
+                    break;
+                }
+            case EUXCallbacksNoParameter.BUTTON_CALLBACK_OPTIONS:
+                {
+
+                    break;
+                }
+            case EUXCallbacksNoParameter.BUTTON_CALLBACK_RETURN_TO_MENU:
+                {
+
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
+    }
+
     void Start()
     {
         
