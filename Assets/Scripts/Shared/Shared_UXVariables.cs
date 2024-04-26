b2aa191272d1f7c3c962de2601e785444e9f4fc1
Harjot Singh <121034602+HarjotSinghGillHL@users.noreply.github.com>
2024-04-26 08:46:30 -0300

    Added UX Classes and UX Level

---
+++ Assets/Scripts/Shared/Shared_UXVariables.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shared_UXVariables : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //UI Variables
+    //Global Variables
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Util/Util_MakeSingleton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Util_MakeSingleton : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    static bool bInitialized = false;
+    void Start()
+    {
+        if (!bInitialized)
+        {
+            GameObject.DontDestroyOnLoad(gameObject);
+            bInitialized = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
