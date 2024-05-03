6ee127dfd82f6d2cdb4aabce3a0ec6a1ce591319
Dylan Ryan <dylanryan431@gmail.com>
2024-05-03 11:26:38 -0300

    Set Box cap on instantiation & froze boxes that hit spikes

--- Assets/Scripts/Level_01_Scripts/BoxDespawn.cs
+++ Assets/Scripts/Level_01_Scripts/BoxDespawn.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class BoxDespawn : MonoBehaviour
 {
+    BoxInstantiation boxInstantiation;
+    GameObject boxSpawn;
+    private void Start()
+    {
+        boxSpawn = GameObject.FindGameObjectWithTag("BoxSpawn");
+        boxInstantiation = boxSpawn.GetComponent<BoxInstantiation>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Box"))
         {
             Destroy(other.gameObject);
+            boxInstantiation.boxCount -= 1;
         }
     }
 }
--- Assets/Scripts/Level_01_Scripts/BoxInstantiation.cs
+++ Assets/Scripts/Level_01_Scripts/BoxInstantiation.cs
@@ -7,6 +7,8 @@ public class BoxInstantiation : MonoBehaviour
     public GameObject boxPrefab;
     public float instantiationInterval = 2f;
     private float timer;
+    public int boxCount = 0;
+    private int maxBoxLimit = 30;
 
     void Start()
     {
@@ -17,11 +19,12 @@ public class BoxInstantiation : MonoBehaviour
     {
         timer -= Time.deltaTime;
 
-        //if the timer reaches zero or less, instantiate the prefab and reset the timer
-        if (timer <= 0f)
+        // If the timer reaches zero or less and box count is less than the limit, instantiate the prefab and reset the timer
+        if (timer <= 0f && boxCount < maxBoxLimit)
         {
             Instantiate(boxPrefab, transform.position, Quaternion.identity);
             timer = instantiationInterval;
+            boxCount++; //increase the box count
         }
     }
 }
