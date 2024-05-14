ab52069e8844f919a4a8767d7738b53605187fa4
Dylan Adams <dylan.adams.2016@outlook.com>
2024-05-14 10:25:59 -0300

    Reworked the Explosive Barrel Script

---
+++ Assets/Scripts/Level_02_Scripts/ElectricalPanel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricalPanel : Explodable
+{
+    [Header("References")]
+    [SerializeField] private GameObject damagedPanel;
+    [SerializeField] private OpenDoor secondDoor;
+    [SerializeField] private Light secondDoorLight;
+
+    public override void Explode()
+    {
+        // When exploded, change the panel model to broken, open the second door, and turn the light green on said door
+        damagedPanel.transform.position = transform.position;
+        damagedPanel.SetActive(true);
+        secondDoor.Open();
+        secondDoorLight.color = Color.green;
+
+        gameObject.SetActive(false);
+    }
+}
